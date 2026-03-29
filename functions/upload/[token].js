515b052a5bf32ccd7d0e66995d46152fd49c5674
kingsleysqi <i@kingsleyqi.com>
2026-03-30 06:16:53 +0800

    update [token].js

--- functions/upload/[token].js
+++ functions/upload/[token].js
@@ -399,7 +399,7 @@ init();
     headers: { 'Content-Type': 'text/html;charset=UTF-8' }
   });
 }
-turn b + ' B';
+return b + ' B';
   if (b < 1048576) return (b / 1024).toFixed(1) + ' KB';
   if (b < 1073741824) return (b / 1048576).toFixed(1) + ' MB';
   return (b / 1073741824).toFixed(2) + ' GB';
